da92afd7760dc1b093e3a67628b9315ec16d17bd
Bidhun <bidhunvaikkath@gmail.com>
2024-07-25 15:36:30 +0530

    Add the player symbol to gameboard dynamically and highlight the player

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,29 +1,40 @@
 import { useState } from "react";
 
 const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({onSlectSquare , activePlayerSymbol}) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-function handleSelectSquare(rowIndex, colIndex){
+  function handleSelectSquare(rowIndex, colIndex) {
     setGameBoard((prevGameBoard) => {
-       const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-       updatedBoard[rowIndex][colIndex] = 'X';
-       return updatedBoard;
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
     });
-}
-  return <ol id="game-board">
-   { gameBoard.map((row, rowIndex) => <li key={rowIndex}>
-    <ol>
-        {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-            <button onClick={() => handleSelectSquare(rowIndex , colIndex)}>{playerSymbol}</button></li> )}
+
+    onSlectSquare();
+  }
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
     </ol>
-   </li>
-    )}
-  </ol>;
+  );
 }
