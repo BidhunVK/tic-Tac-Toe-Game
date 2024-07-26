b74e3a3eba8ea03eb4806197767d4867fa0f40a9
Bidhun <bidhunvaikkath@gmail.com>
2024-07-26 17:15:21 +0530

    restart match functionality

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
-  let gameBoard = initialGameBoard;
+  let gameBoard = [...initialGameBoard.map(innerArray => [...innerArray])];
 
   for (const turn of gameTurns) {
     const { square, player } = turn;
@@ -70,6 +70,10 @@ function App() {
     });
   }
 
+    function handleRestart(){
+      setGameTurns([]);
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -85,7 +89,7 @@ function App() {
             isActive={activePlayer === "O" ? true : false}
           />
         </ol>
-        {(winner || hasDraw) && <GameOver winner={winner} />}
+        {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard onSlectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
